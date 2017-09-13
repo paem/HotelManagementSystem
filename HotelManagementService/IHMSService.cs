93da56f69c2cee2dfacbe9b5ada4d03c5f0fb20a
Patrik Emilsson <patrik_emilsson@hotmail.com>
2017-09-13 16:11:58 +0200

    Fixed references

--- HotelManagementService/IHMSService.cs
+++ HotelManagementService/IHMSService.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
-using HMS.Model;
+using HMS.Models;
 
 namespace HotelManagementService
 {
