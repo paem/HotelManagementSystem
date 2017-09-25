fc2de2eb1efea82ef73ad10d4510cb80c2bd1a3d
Vidar Asp Johansson <vidarcsgo@gmail.com>
2017-09-25 17:03:46 +0200

    UWP AutoSuggestBox

--- GUI.AdminApp/MainPage.xaml.cs
+++ GUI.AdminApp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,7 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using GUI.AdminApp.HMSServiceReference;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -23,94 +24,44 @@ namespace GUI.AdminApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        HMSServiceReference.HMSServiceClient HMSClient = new HMSServiceReference.HMSServiceClient();
+        private readonly HMSServiceClient HMSClient = new HMSServiceClient();
 
+        /*
         //Skriver ut alla användare
         private async void LoadCustomers()
         {
-
             var customerList = await HMSClient.GetCustomersAsync();
-
             userList.ItemsSource = customerList; 
-
         }
-    
+       */
         public MainPage()
         {
-
-           LoadCustomers();
-           
-
+            //LoadCustomers();
             this.InitializeComponent();
         }
-        private void mySearchBox_QuerySubmitted(SearchBox sender,
-            SearchBoxQuerySubmittedEventArgs args)
-        {
-            string keyword = mySearchBox.QueryText;
 
-            var customerList = HMSClient.GetCustomersAsync();
-
-            var searchResult = customerList.Result.Where(name => name.ToString().Contains(keyword));
-
-            userList.ItemsSource = searchResult;
-            
-
-         //   this.Frame.Navigate(typeof(SearchResultPage), args.QueryText);
-        }
-     /*   class SearchResultPage
-        {
-
-        }
-        */
-
-
-
-
-        /*
-        //Kanske ska använda AUTOSUGGESTBOX istället för SearchBOX
-
-        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
-        {
-            // Only get results when it was a user typing,
-            // otherwise assume the value got filled in by TextMemberPath
-            // or the handler for SuggestionChosen.
-            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
-            {
-                //Set the ItemsSource to be your filtered dataset
-                //sender.ItemsSource = dataset;
-             
-              
-            //  var customerList = HMSClient.GetCustomersAsync();
-
-              //  userList.ItemsSource = customerList;
-            }
-        }
-
-
-        private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
-        {
-            // Set sender.Text. You can use args.SelectedItem to build your text string.
-        }
-
-
-        private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        private void AutoSuggBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
         {
             if (args.ChosenSuggestion != null)
             {
-                // User selected an item from the suggestion list, take an action on it here.
+                AutoSuggBox.Text = args.ChosenSuggestion.ToString();
             }
-            else
+        }
+
+        private void AutoSuggBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            var selectedItem = args.SelectedItem.ToString();
+            sender.Text = selectedItem;
+        }
+
+        private void AutoSuggBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
             {
-                // Use args.QueryText to determine what to do.
-                string keyword = args.QueryText;
-
                 var customerList = HMSClient.GetCustomersAsync();
-
-                var searchResult = customerList.Result.Where(name => name.ToString().Contains(keyword));
-
+                var searchResult = customerList.Result.Where(name => (name.CustomerFName ?? "").ToLower().Contains(sender.Text));
                 userList.ItemsSource = searchResult;
             }
         }
-        */
     }
 }
